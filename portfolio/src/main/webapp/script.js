0985772438d4daf9b3382eefb5f6d6319afcbb36
Maliha <malihabakhshi01@gmail.com>
2020-07-12 22:31:42 +0530

    work on javascript file

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,30 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+$(document).ready(function(){
+    const navToggle=document.querySelector('.nav-toggle');
+    const navLinks=document.querySelectorAll('.nav-link');
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+    navToggle.addEventListener('click', function(){
+        document.body.classList.toggle('nav-open');
+    });
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(){
+            document.body.classList.remove('nav-open');
+        });
+    });
+
+    $('#up').on('click', function(){
+        $('html, body').animate({
+            scrollTop:0
+        }, 2000)
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+});
+
